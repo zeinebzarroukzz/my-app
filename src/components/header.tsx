2e2401cb59ec5bb4406cf32246f681b80fed972b
zarrouk zeineb <zeinebzarroukzz@gmail.com>
2023-10-02 20:44:29 +0100

    first commit

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './Images/Logo.svg';
+
+const header = () => {
+  return (
+    <img src={logo}  className="logo" alt="My Website Logo" />
+  )
+}
+
+export default header
+
+
+
---
+++ src/components/nav.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const nav = () => {
+  return (
+    <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/menu">Menu</a></li>
+      <li><a href="/reservations">Reservations</a></li>
+      <li><a href="/orderOnline">Order online</a></li>
+      <li><a href="/login">Login</a></li>
+    </ul>
+  </nav>
+  )
+}
+
+export default nav
